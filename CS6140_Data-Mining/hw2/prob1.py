22736cd84c0d7b39dc557348c1007ea5c16519db
cmertin <cmertin@cs.utah.edu>
2017-02-12 11:39:15 -0700

    Finished HW2

--- CS6140_Data-Mining/hw2/prob1.py
+++ CS6140_Data-Mining/hw2/prob1.py
@@ -1,5 +1,8 @@
 from __future__ import print_function, division
 import numpy as np
+import math
+from random import shuffle
+import itertools
 
 def ReadFile(filename):
     lines = [line.rstrip('\n') for line in open(filename)]
@@ -128,42 +131,133 @@ def Print_JS_Table(file_list, js_matrix, caption=None):
     print("\\end{table}")
     print("\n")
 
+def KGrams(file_list):
+    char2_list = []
+    char3_list = []
+    word2_list = []
+
+    for file in files:
+        words, string = ReadFile(file)
+
+        grams = K_Grams_Chars(string, k=2)
+        char2_list.append(grams)
+
+        grams = K_Grams_Chars(string, k=3)
+        char3_list.append(grams)
+
+        grams = K_Grams_Words(words, k=2)
+        word2_list.append(grams)
+
+    # Prints out the table in LaTeX format for the k2-grmas and k3-gram
+    Print_KGram_Table(files, char2_list, char3_list, word2_list)
+
+    js_matrix = JacardSimilarities(files, char2_list)
+    Print_JS_Table(files, js_matrix, caption="$k_{2}$-Character Jacard Similarities")
+
+    js_matrix = JacardSimilarities(files, char3_list)
+    Print_JS_Table(files, js_matrix, caption="$k_{3}$-Character Jacard Similarities")
+
+    js_matrix = JacardSimilarities(files, word2_list)
+    Print_JS_Table(files, js_matrix, caption="$k_{2}$-Word Jacard Similarities")
+
+def MinHash(file1, file2, t, k_=3):
+    min_hash = {}
+    JS = 0
+    words, string = ReadFile(file1)
+    f1_hash = K_Grams_Chars(string, k=k_)
+    
+    words, string = ReadFile(file2)
+    f2_hash = K_Grams_Chars(string, k=k_)
+
+    union = []
+    
+    for key in f1_hash.keys():
+        union.append(key)
+    for key in f2_hash.keys():
+        if key not in union:
+            union.append(key)
+    
+    for item in union:
+        if item in f1_hash and item in f2_hash:
+            min_hash[item] = [1,1]
+        elif item in f1_hash and item not in f2_hash:
+            min_hash[item] = [1,0]
+        else:
+            min_hash[item] = [0,1]
+            
+    # Permute the min has by row and calculate JS
+    for i in range(t):
+        m1 = -1
+        m2 = -1
+        Xt = 0
+        j = 0
+        rand_list = [i for i in range(len(union))]
+        shuffle(rand_list)
+        
+        while m1 is -1 and m2 is -1:
+            j += 1
+            elm = min_hash[union[rand_list[j]]]
+            if elm[0] is 1:
+                m1 = union[rand_list[i]]
+            if elm[1] is 1:
+                m2 = union[rand_list[i]]
+            
+        if m1 is m2:
+            Xt = 1
+        
+        JS += (1/float(t)) * Xt
+    return JS
+
+def MinHash_FN(t_list, f1, f2):
+    last_t = t_list[len(t_list)-1]
+    print("\\begin{table}[H]")
+    print("\\centering")
+    print("\\begin{tabular}{lr}")
+    print("\\hline\\hline")
+    print("$t$ & $J_{S}$\\\\")
+    print("\\hline")
+    for t in t_list:
+        JS = MinHash(f1, f2, t)
+        JS_str = "%.3f" % JS
+        if t is not last_t:
+           print(str(t) + " & " + JS_str + "\\\\")
+        else:
+            print(str(t) + " & " + JS_str + "\\\\")
+            print("\\hline")
+    print("\\end{tabular}")
+    print("\\end{table}")
+        
+def ProbSimilarity(s, b, r):
+    return 1 - math.pow(1 - math.pow(s, b), r)
+    
+def ProbHash_FN(files, t, b=5.539, r=28.886):
+    print("\\begin{table}[H]")
+    print("\\centering")
+    print("\\begin{tabular}{lr}")
+    print("\\hline\\hline")
+    print("$f(D_{i}, D_{j})$ & Probability$/100$\\\\")
+    print("\\hline")
+    for i in range(len(files)):
+        for j in range(len(files)):
+            if j <= i:
+                continue
+            JS = MinHash(files[i], files[j], t)
+            JS = ProbSimilarity(JS, b, r)
+            JS_str = "%.3f" % JS
+            print("$f(${\\tt " + files[i] + "}$,$ " + "{\\tt " + files[j] + "}$)$ & " + JS_str + "\\\\")
+    print("\\hline")
+    print("\\end{tabular}")
+    print("\\end{table}")
+
 # Start of the main program
                   
 files = ["D" + str(i) + ".txt" for i in range(1,5)]
+t_list = [20, 60, 150, 300, 600]
+t_list2 = [500, 700, 900, 1100, 1300, 1500, 1700, 1900]
+t = 160
+# KGrams(files)
 
-words, string = ReadFile(files[0])
-char2_list = []
-char3_list = []
-word2_list = []
+#MinHash_FN(t_list, files[0], files[1])
+#MinHash_FN(t_list2, files[0], files[1])
 
-for file in files:
-    words, string = ReadFile(file)
-
-    grams = K_Grams_Chars(string, k=2)
-
-    char2_list.append(grams)
-
-    grams = K_Grams_Chars(string, k=3)
-
-    char3_list.append(grams)
-
-    grams = K_Grams_Words(words, k=2)
-
-    word2_list.append(grams)
-
-# Prints out the table in LaTeX format for the k2-grmas and k3-gram
-Print_KGram_Table(files, char2_list, char3_list, word2_list)
-
-js_matrix = JacardSimilarities(files, char2_list)
-
-Print_JS_Table(files, js_matrix, caption="$k_{2}$-Character Jacard Similarities")
-
-js_matrix = JacardSimilarities(files, char3_list)
-
-Print_JS_Table(files, js_matrix, caption="$k_{3}$-Character Jacard Similarities")
-
-js_matrix = JacardSimilarities(files, word2_list)
-
-Print_JS_Table(files, js_matrix, caption="$k_{2}$-Word Jacard Similarities")
-
+ProbHash_FN(files, t)
