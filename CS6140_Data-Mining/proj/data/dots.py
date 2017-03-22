d0225f39c4bd8bf2c728a09b266c569b905293fe
Christopher Mertin <cmertin@cs.utah.edu>
2017-03-21 19:48:32 -0600

    Extracted SIFT features from averages and plot

---
+++ CS6140_Data-Mining/proj/data/dots.py
@@ -0,0 +1,36 @@
+from __future__ import print_function, division
+import numpy as np
+import matplotlib.pyplot as plt
+
+def ReadDots(filename):
+    x = []
+    y = []
+    lines = [line.strip() for line in open(filename)]
+
+    for line in lines:
+        temp = line.split(',')
+        x.append(temp[0])
+        y.append(600 - int(temp[1]))
+
+    return x, y
+
+
+city = "Provo"
+filename = "_avg."
+ext = ["jpg", "dat"]
+
+dots_file = city + filename + ext[1]
+
+x_p, y_p = ReadDots(dots_file)
+
+city = "Seattle"
+dots_file = city + filename + ext[1]
+
+x_s, y_s = ReadDots(dots_file)
+
+plt.scatter(x_p, y_p, c="red", label="Mormon")
+plt.scatter(x_s, y_s, c="blue", label="Non-Mormon")
+plt.legend(loc="best")
+plt.savefig("two_faces.pdf", bbox_inches="tight")
+plt.show()
+
