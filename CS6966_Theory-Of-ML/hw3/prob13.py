fc474c7daf2f777bd411cbbdab04b2b7104cf164
cmertin <cmertin@cs.utah.edu>
2017-04-09 13:41:02 -0600

    Halfway done

--- CS6966_Theory-Of-ML/hw3/prob13.py
+++ CS6966_Theory-Of-ML/hw3/prob13.py
@@ -29,8 +29,8 @@ def NN(x):
 
 vals = []
 x = []
-random.seed(0)
-n = 1000
+#random.seed(0)
+n = 500
 
 for i in range(n):
     x.append(random.uniform(-1, 2))
@@ -39,19 +39,27 @@ for x_ in x:
     vals.append(NN(x_))
 
 plt.scatter(x, vals)
-plt.ylim([-0.05,1])
+plt.ylim([-0.05,1.05])
 plt.xlim([-1,2])
 plt.xlabel("x")
 plt.ylabel("NN(x)")
 plt.title("Single Layer Network")
 plt.savefig("single_layer-13.pdf", bbox_inches="tight")
+plt.show()
 
 x = deepcopy(vals)
 vals = []
 for x_ in x:
     vals.append(NN(x_))
 
+plt.clf()
+plt.ylim([-0.05,1.05])
+plt.xlim([-0.05,1.05])
+plt.xlabel("x")
+plt.ylabel("NN(NN(x))")
+plt.title("Single stacked network")
 plt.scatter(x, vals)
+plt.savefig("stacked-13.pdf", bbox_inches="tight")
 plt.show()
 
 
